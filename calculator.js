3b8361aaa0ac30210087166cab48bc0a95a8f6a2
Benjamin Stevens <B3nbinoh@gmail.com>
2023-10-19 15:38:01 -0600

    +calculator +psuedocode

---
+++ calculator.js
@@ -0,0 +1,74 @@
+// Ask the user for the first number.
+// Ask the user for the second number.
+// Ask the user for an operation to perform.
+// Perform the operation on the two numbers.
+// Print the result to the terminal.
+
+let rlSync = require("readline-sync");
+
+function prompt(message) {
+  message = `=>` + message;
+  return message;
+}
+function invalidNumber(number) {
+  return number.trimStart() === "" || Number.isNaN(Number(number));
+}
+
+console.log("Welcome to the Calculator!");
+let firstNum = rlSync.question(prompt("What's the first number?\n")); //Get first number and test for validation
+while (invalidNumber(firstNum)) {
+  firstNum = rlSync.question(
+    prompt(`Hmm... that doesn't look like a valid number.\n`)
+  );
+}
+
+let secondNum = rlSync.question(prompt("What's the second number?\n")); //Get second number and test for validation
+while (invalidNumber(secondNum)) {
+  secondNum = rlSync.question(
+    prompt(`Hmm... that doesn't look like a valid number.\n`)
+  );
+}
+
+let operation = rlSync.question(
+  prompt(
+    "What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide\n"
+  )
+); //Get operation
+while (!["1", "2", "3", "4"].includes(operation)) {
+  operation = rlSync.question(prompt("Must choose 1, 2, 3 or 4\n"));
+}
+
+let result;
+
+function add(num1, num2) {
+  //Operation functions
+  return Number(num1) + Number(num2);
+}
+function subtract(num1, num2) {
+  return Number(num1) - Number(num2);
+}
+function multiply(num1, num2) {
+  return Number(num1) * Number(num2);
+}
+function divide(num1, num2) {
+  return Number(num1) / Number(num2);
+}
+
+switch (operation) {
+  case "1":
+    result = add(firstNum, secondNum);
+    console.log(prompt(`The result is: ${result}`));
+    break;
+  case "2":
+    result = subtract(firstNum, secondNum);
+    console.log(prompt(`The result is: ${result}`));
+    break;
+  case "3":
+    result = multiply(firstNum, secondNum);
+    console.log(prompt(`The result is: ${result}`));
+    break;
+  case "4":
+    result = divide(firstNum, secondNum);
+    console.log(prompt(`The result is: ${result}`));
+    break;
+}
---
+++ psuedocode.js
@@ -0,0 +1,44 @@
+/** Given two numbers
+ *  Add the two numbers together and return the result/sum
+ */
+
+/**Given an array of strings
+ *
+ * Create a resulting string variable
+ *
+ * Iterate through the entire array
+ * - Each iteration add the value at the current index to the resulting string
+ *
+ * Return the resulting string variable
+ */
+
+/**Given an array of integers
+ *
+ * Create a resulting array variable
+ *
+ * Iterate through every other element of the given array
+ * -.push() the current element to the resulting array
+ *
+ * Return the resulting array
+ */
+
+/**Given a string and a character
+ *
+ * Create a counter variable starting at zero to determine which occurence of the given character we are on
+ *
+ * Iterate through the string
+ * -Test whether the current character is the same as the given character
+ * -If it is then add one the the counter
+ * -Once the counter equals three stop iterating and return the current index
+ * -If the counter does not reach three by the end of the string stop iterating and return null
+ */
+
+/**Given two arrays
+ *
+ * Create a new resulting array
+ *
+ * Iterate through both entire arrays
+ * -.push() The current element from the first, then the second array to the resulting array
+ *
+ * Return the resulting array
+ */
